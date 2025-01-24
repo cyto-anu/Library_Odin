5f84fe633ba2662131e66d907758b0f470bc4c2f
Anu <anu.cyto@gmail.com>
2025-01-23 22:55:41 -0500

    added function to display book info on page via console

---
+++ script.js
@@ -0,0 +1,48 @@
+const container = document.querySelector(".container");
+
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+
+};
+
+function addBookToLibrary(title, author, pages, read) {
+    const newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+};
+
+addBookToLibrary("Harry Potter", "JK Rowling", 1128, "read")
+addBookToLibrary("Romeo and juliet", "Shakespeare", 184, "read")
+addBookToLibrary("Steve Jobs", "Walter Issaicson", 592, "read")
+
+function loopThru() {
+    myLibrary.forEach(element => {
+
+        const titleDiv = document.createElement("div");
+        titleDiv.classList.add("book");
+        container.appendChild(titleDiv);
+        titleDiv.textContent = element.title;
+
+        const authorDiv = document.createElement('div');
+        authorDiv.classList.add("author");
+        titleDiv.appendChild(authorDiv);
+        authorDiv.textContent = element.author;
+
+        const pagesDiv = document.createElement('div');
+        pagesDiv.classList.add("pages");
+        titleDiv.appendChild(pagesDiv);
+        pagesDiv.textContent = element.pages;
+
+        const readDiv = document.createElement('div');
+        readDiv.classList.add("read");
+        titleDiv.appendChild(readDiv);
+        readDiv.textContent = element.read;
+
+    });
+};
+
+loopThru();
